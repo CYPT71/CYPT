d727057a024cc6ace45183c8614d5726c1787e5d
CYPT71 <51490865+CYPT71@users.noreply.github.com>
2019-06-09 12:31:42 +0200

    Add files via upload

--- copiercoller.bat
+++ copiercoller.bat
@@ -1,47 +1,76 @@
-@echo off
-
+@echo Off 
+                
 Title "backup off backup"
 
+cd C:\Users\cyprien\Desktop\test                                
+
+if not Exist config.txt (
+    echo >>config.txt FileId=
+    echo >>config.txt Execution script=
+    echo >>config.txt Save Location=
+    start config.txt
+    pause = null                
+    )
+    
+
+for /f "delims=" %%a in (config.txt) do set %%a
+set id=%FileId%
+for /f "delims=" %%a in (config.txt) do set %%a
+set Ex=%Execution script%
+for /f "delims=" %%a in (config.txt) do set %%a
+set SaveL=%Save Location%
+
                 
-cd %userprofile%\Desktop\               
+cd %Ex%           
                 
-MKDIR "backupcopy"
+MKDIR backupcopy                
+gdrive-windows-x64.exe sync upload %Ex%backupcopy %id%
 
 
-:tt
-    
-    
-    
-    cd %userprofile%\Desktop\
 
-    set $path=%userprofile%\Desktop\backupcopy
+:tt                            
+     
+    cd %Ex%             
+
+    set $path=%Ex%\backupcopy                   
     for %%a in (%$path%) do set $Fichier=%%~na
-            echo %$Fichier%
+    echo %$Fichier%             
             del  %$Fichier% /Q
             goto $$2
         
-:$$2
+  :$$2                              
+  
 
-    cd %userprofile%\AppData\Local\FactoryGame\Saved\SaveGames
-
-
-    set $chemin=%userprofile%\AppData\Local\FactoryGame\Saved\SaveGames
-    for /f "delims=" %%a in ('dir %$chemin% /aa/b/o-d') do (set $f="%%a"
+    cd %Save Location%              
+    set $chemin=%Save Location%                 
+    
+                    
+    
+    for /f "delims=" %%a in ('dir %$chemin% /aa/b/o-d') do (set $f=%%a              
                                                goto:next)
-    :next
+    :next            
     
-    echo le dernier fichier cr?? est : %$f%
-    xcopy %$f% %userprofile%\Desktop\backupcopy /-Y /Q /Y
-    cd %userprofile%\Desktop\backupcopy
-    gdrive-windows-x64.exe sync upload C:\Users\cyprien\Desktop\backupcopy **fileid**
+    echo le dernier fichier cree est : %$f%
     
-    TIMEOUT /T 300
+    cd %Ex%
+                    
+    xcopy %$f% %Ex%\backupcopy /-Y /Q /Y
+    
+    
+    
+    cd %Ex%
+    
+    echo %Ex%\backupcopy\%$f%
+    echo %id%               
+   gdrive-windows-x64.exe sync upload %Ex%\backupcopy\%$f% %id%                                        
+                                    
+                                    
+    
+                             
+                    
+   TIMEOUT /T 300           
 
     cls             
 
-    
-        
-
-
-goto tt
+    goto tt             
 
